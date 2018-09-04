429c715e88013c476267688c93248ea3284553e7
ria10 <otatakuma0714@gmail.com>
2018-09-05 08:13:03 +0900

    spaceキーで前進する機能を追加

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+
+    }
+}

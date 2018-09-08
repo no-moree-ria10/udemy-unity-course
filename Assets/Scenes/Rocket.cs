3d4b189dcbf89466d91693bde04a5d56812e59c7
ria10 <otatakuma0714@gmail.com>
2018-09-08 19:49:26 +0900

    ・パラメータをinpectorから操作できるよう追加 ・freezeRotationを追加（回転操作時の急速な動作を避けるため）

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,27 +5,44 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rigidbody;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 1;
+
+    Rigidbody rigidBody;
     AudioSource audioSource;
 
 
-	// Use this for initialization
-	void Start () {
-        rigidbody = GetComponent<Rigidbody>();
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("dead...");
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying)
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
@@ -34,14 +51,25 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * 100);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
+        rigidBody.freezeRotation = false; // take manual control of rotation
     }
-}
+
+
+    }
